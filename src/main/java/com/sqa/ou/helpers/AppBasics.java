cc78faaf0f7bcebc44921262c2e75211b7179143
Olena U <olena.umanska1@gmail.com>
2017-01-19 18:56:36 -0800

    Update src

--- src/main/java/com/sqa/ou/helpers/AppBasics.java
+++ src/main/java/com/sqa/ou/helpers/AppBasics.java
@@ -42,6 +42,36 @@ public class AppBasics {
 		return response;
 	}
 
+	public static int requestIntFromUser(String question) {
+		return requestIntFromUser(question, 0, 0);
+	}
+
+	public static int requestIntFromUser(String question, int min, int max) {
+		String input;
+		int result = 0;
+		boolean isNotValid = true;
+		while (isNotValid) {
+			try {
+				System.out.print(question + " ");
+				input = scanner.nextLine();
+				result = Integer.parseInt(input);
+				if (min != 0 && max != 0) {
+					if (result <= max && result >= min) {
+						isNotValid = false;
+					} else {
+						throw new NumberNotInRangeException();
+					}
+				}
+				isNotValid = false;
+			} catch (NumberFormatException e) {
+				System.out.println("You have not entered a correct number value.");
+			} catch (NumberNotInRangeException e) {
+				System.out.println("You have not entered a number within the range of " + min + " and " + max + ".");
+			}
+		}
+		return result;
+	}
+
 	public static void tellUserWhatDayIsToday() {
 		Calendar now = Calendar.getInstance();
 		System.out.println("Current date : " + (now.get(Calendar.MONTH) + 1) + "-" + now.get(Calendar.DATE) + "-"
